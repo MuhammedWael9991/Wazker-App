ebf3d2d0ca8ab4c752288d38301e8360370f82d1
MuhammedWael9991 <mw26200400@gmail.com>
2025-07-19 19:10:30 +0300

    Refactor PrayerBox and PrayerInfo components to remove unused imports

--- app/src/main/java/com/wazker/presentation/components/PrayerBox.kt
+++ app/src/main/java/com/wazker/presentation/components/PrayerBox.kt
@@ -1,11 +1,8 @@
 package com.wazker.presentation.components
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
