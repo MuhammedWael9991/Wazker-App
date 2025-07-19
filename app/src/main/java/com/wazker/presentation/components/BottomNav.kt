62ae070168f5a3635e5ef50ea781a9f5cb9f5a4d
Mid0oo <medoprof03@gmail.com>
2025-07-19 20:16:41 +0300

    Add Bottom Navigation Bar

---
+++ app/src/main/java/com/wazker/presentation/components/BottomNav.kt
@@ -0,0 +1,97 @@
+package com.wazker.presentation.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.wazker.R
+import com.wazker.presentation.design_system.theme.Theme
+
+
+@Composable
+fun BottomNavBar(
+    selectedIndex: Int,
+    onItemSelected: (Int) -> Unit,
+    modifier: Modifier = Modifier,
+
+    ) {
+    val items = listOf(
+        R.drawable.ic_mosqe,
+        R.drawable.ic_duaa,
+        R.drawable.ic_tasbih,
+        R.drawable.ic_prayer_times
+    )
+
+    Box(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(horizontal = 24.dp, vertical = 24.dp)
+
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+
+        ) {
+            items.forEachIndexed { currentIndex, item ->
+                Box(
+                    modifier = Modifier
+                        .size(42.dp)
+                        .clip(shape = RoundedCornerShape(size = 16.dp))
+                        .background(
+                            if (currentIndex == selectedIndex)
+                                Color.White.copy(alpha = 0.15f)
+                            else
+                                Color.Transparent
+                        )
+                        .clickable { onItemSelected(currentIndex) },
+                    contentAlignment = Alignment.Center
+                ) {
+                    Icon(
+                        painter = painterResource(id = item),
+                        contentDescription = null,
+                        tint = Color.White,
+                        modifier = Modifier.size(24.dp)
+                    )
+
+                }
+
+            }
+        }
+    }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun BottomNavPreview() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(
+                brush = Brush.verticalGradient(
+                    colors = Theme.colors.primaryGradient.colors)),
+        verticalArrangement = Arrangement.Bottom,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        BottomNavBar(selectedIndex = 2, onItemSelected = {})
+
+    }
+}
