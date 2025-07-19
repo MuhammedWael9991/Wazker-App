fe614e2a3d788f40f7a3f7811ede1c08ef8398d3
MuhammedWael9991 <mw26200400@gmail.com>
2025-07-19 19:10:07 +0300

    Add PrayerInfo component to display prayer names and times

---
+++ app/src/main/java/com/wazker/presentation/components/PrayerInfo.kt
@@ -0,0 +1,40 @@
+package com.wazker.presentation.components
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun PrayerInfo(
+    name: List<String>,
+    time: List<String>,
+    modifier: Modifier = Modifier,
+) {
+    Column(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp)
+    ) {
+        name.forEach {
+            PrayerBox(
+                name = it,
+                time = time[name.indexOf(it)]
+            )
+        }
+    }
+}
+
+
+@Preview
+@Composable
+private fun Preview(){
+    PrayerInfo(
+        name = listOf("Fajr", "Dhuhr", "Asr", "Maghrib", "Isha"),
+        time = listOf("05:00 AM", "12:30 PM", "03:45 PM", "06:15 PM", "07:30 PM")
+    )
+}
